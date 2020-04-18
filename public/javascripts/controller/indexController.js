f28c1d457795e41fafac7ee4b121d57a0ef7ae06
Celal <cllakyz@hotmail.com>
2020-04-19 02:17:00 +0300

    yeni kullanıcı girişinde sockete bağlanması işlemi

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,14 +1,28 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-    const connectOptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelay: 500,
-    };
 
-    indexFactory.connectSocket('http://localhost:30000', connectOptions)
-        .then((socket) => {
-            console.log('Bağlantı Yapıldı.', socket);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    $scope.init = () => {
+        const username = prompt('Please enter username');
+
+        if (username)
+            initSocket(username);
+        else
+            return false;
+    };
+    
+    function initSocket(username) {
+        const connectOptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelay: 500,
+        };
+
+        indexFactory.connectSocket('http://localhost:3000', connectOptions)
+            .then((socket) => {
+                //console.log('Bağlantı Yapıldı.', socket);
+                socket.emit('newUser', { username })
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
 }]);
