08941c889cf10923e02c6ceec5e6548a9743b3d3
Celal <cllakyz@hotmail.com>
2020-04-19 02:35:29 +0300

    yeni kullanıcı girişinde mesaj gösterilmesi

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,5 +1,7 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
+    $scope.messages = [];
+
     $scope.init = () => {
         const username = prompt('Please enter username');
 
@@ -18,7 +20,18 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
         indexFactory.connectSocket('http://localhost:3000', connectOptions)
             .then((socket) => {
                 //console.log('Bağlantı Yapıldı.', socket);
-                socket.emit('newUser', { username })
+                socket.emit('newUser', { username });
+
+                socket.on('newUser', (data) => {
+                    const messageData = {
+                        type: 0, // info
+                        username: data.username,
+
+                    };
+
+                    $scope.messages.push(messageData);
+                    $scope.$apply();
+                });
             })
             .catch((err) => {
                 console.log(err);
