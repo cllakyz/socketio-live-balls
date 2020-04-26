6a27108388c750b8c4b948776b5820b710d26996
Celal <cllakyz@hotmail.com>
2020-04-26 13:51:11 +0300

    düzeltme

--- public/javascripts/services/configFactory.js
+++ public/javascripts/services/configFactory.js
@@ -1,10 +1,8 @@
-const env = require('../../../config/env.json')[process.env.NODE_ENV || 'development'];
-
 app.factory('configFactory', ['$http', ($http) => {
     const getConfig = () => {
         return new Promise((resolve, reject) => {
             $http
-                .get(env.path + 'getEnv')
+                .get('/getEnv')
                 .then((data) => {
                     resolve(data);
                 })
